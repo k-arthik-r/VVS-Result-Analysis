badeacb84b580db7d70c0e8e59e9c83beb9bf9f5
k-arthik-r <karthikclgid@gmail.com>
2024-04-19 22:10:25 +0530

    initial commit

---
+++ Dashboard.py
@@ -0,0 +1,224 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+import plotly.express as px
+import pandas as pd
+
+
+st.set_page_config(page_title="VVS", layout="wide", initial_sidebar_state="collapsed")
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+df = pd.read_csv("students.csv")
+specified_columns = ['Name', 'Language', 'English', 'Maths', 'Science', 'Social', 'Percentage', 'Result',  'Gender', 'Section',]
+new_df = df[specified_columns].copy()
+
+result_counts = df['Result'].value_counts()
+result_gender = df['Gender'].value_counts()
+numOfStudents = len(df)
+
+st.title("Vidyavardhaka School Result Analysis")
+st.markdown("<style>div.block-container{padding-top:1rem;}</style>", unsafe_allow_html=True)
+
+
+
+st.text("")
+
+col0_1, col0_2, col0_3 = st.columns([8, 2, 2])
+
+with col0_2:
+    if st.button("Section A Analysis"):
+        switch_page("Section_A")
+
+with col0_3:
+    if st.button("Section B Analysis"):
+        switch_page("Section_B")   
+    
+
+
+col1, col2 = st.columns([1, 2], gap="medium")
+
+with col1:
+    st.subheader("Overall Results")
+    fig = px.pie(values=result_counts.values, names=result_counts.index, hole=0.5)
+    fig.update_traces(text = result_counts.index, textposition="outside")
+    st.plotly_chart(fig, use_container_width=True)
+
+with col2:
+    st.subheader("Student Details")
+    container = st.container(border=True)
+    container.write(f"**Total Number of Students: {numOfStudents}**")
+
+    col2_1, col2_2 = st.columns((2))
+    
+    with col2_1:
+        container2_1 = st.container(border=True)
+        container2_1.write("**Boys**")
+        container2_1.write(f"**{result_gender['Male']}**")
+
+    with col2_2:
+        container2_2 = st.container(border=True)
+        container2_2.write("**Girls**")
+        container2_2.write(f"**{result_gender['Female']}**")
+
+    with col2_1:
+        container2_1 = st.container(border=True)
+        container2_1.write("**Total Number of Pass**")
+        container2_1.write(f"**{result_counts['Pass']}**")
+
+    with col2_2:
+        container2_2 = st.container(border=True)
+        container2_2.write("**Total Number of Fails**")
+        container2_2.write(f"**{result_counts['Fail']}**")
+
+
+st.subheader("Overall Grades")
+
+col3_1, col3_2, col3_3, col3_4, col3_5, col3_6 = st.columns((6))
+col3list = [col3_1, col3_2, col3_3, col3_4, col3_5, col3_6]
+
+students_90_above = df[df['Percentage'] >= 90]
+num_students_90_above = len(students_90_above)
+
+students_80_above = df[(df['Percentage'] >= 80) & (df['Percentage'] < 90)]
+num_students_80_above = len(students_80_above)
+
+students_70_above = df[(df['Percentage'] >= 70) & (df['Percentage'] < 80)]
+num_students_70_above = len(students_70_above)
+
+students_60_above = df[(df['Percentage'] >= 60) & (df['Percentage'] < 70)]
+num_students_60_above = len(students_60_above)
+
+students_40_above = df[(df['Percentage'] >= 40) & (df['Percentage'] < 60)]
+num_students_40_above = len(students_40_above)
+
+students_40_below = df[df['Percentage'] < 40]
+num_students_40_below = len(students_40_below)
+
+
+for i in col3list:
+    with i:
+        container = st.container(border=True)
+
+        if i is col3_1:
+            container.write("**S Grade**")
+            container.write(f"**{num_students_90_above}**")
+
+        if i is col3_2:
+            container.write("**A Grade**")
+            container.write(f"**{num_students_80_above}**")
+
+        if i is col3_3:
+            container.write("**B Grade**")
+            container.write(f"**{num_students_70_above}**")
+
+        if i is col3_4:
+            container.write("**C Grade**")
+            container.write(f"**{num_students_60_above}**")
+
+        if i is col3_5:
+            container.write("**D Grade**")
+            container.write(f"**{num_students_40_above}**")
+
+        if i is col3_6:
+            container.write("**X Grade**")
+            container.write(f"**{num_students_40_below}**")
+
+st.text("")
+
+col4= st.columns((1))
+
+with col4[0]:
+    st.subheader("Conmaprision of Average score between Section A and Section B")
+    numeric_columns = df.select_dtypes(include=['int64', 'float64']).columns
+    avg_scores = df.groupby('Section')[numeric_columns].mean().reset_index()
+    avg_scores_melted = avg_scores.melt(id_vars='Section', var_name='Subject', value_name='Average Score')
+    fig_line_section = px.line(avg_scores_melted, x='Subject', y='Average Score', color='Section',
+                            labels={'Average Score': 'Average Score', 'Subject': 'Subject', 'Section': 'Section'})
+    st.plotly_chart(fig_line_section, use_container_width=True)
+
+def barChart(lan):
+    pass_sub = df[df['Pass/Fail'] == 'Pass']
+    pass_count = pass_sub.groupby('Section').size()
+    sections = ['A', 'B']
+    pass_count = pass_count.reindex(sections, fill_value=0).reset_index(name='Pass Count')
+    fig = px.bar(pass_count, x='Section', y='Pass Count', 
+             title=f'{lan}',
+             labels={'Pass Count': 'Pass Count', 'Section': 'Section'})
+    fig.update_layout(yaxis=dict(range=[0, 20]))
+
+    st.plotly_chart(fig, use_container_width=True)
+
+st.subheader("Subject Wise Result Analysis")
+
+col5_1, col5_2, col5_3 = st.columns(3)
+
+with col5_1:
+    df['Pass/Fail'] = df['Language'].apply(lambda x: 'Pass' if x >= 40 else 'Fail')
+    barChart(lan = "Language")
+
+with col5_2:
+    df['Pass/Fail'] = df['English'].apply(lambda x: 'Pass' if x >= 40 else 'Fail')
+    barChart(lan = "English")   
+
+with col5_3:
+    df['Pass/Fail'] = df['Maths'].apply(lambda x: 'Pass' if x >= 40 else 'Fail')
+    barChart(lan = "Maths")
+
+with col5_1:
+    df['Pass/Fail'] = df['Science'].apply(lambda x: 'Pass' if x >= 40 else 'Fail')
+    barChart(lan = "Science")  
+
+with col5_2:
+    df['Pass/Fail'] = df['Social'].apply(lambda x: 'Pass' if x >= 40 else 'Fail')
+    barChart(lan = "Social Science") 
+
+st.subheader("Leader Board")
+
+pass_students_section_a = df[(df['Result'] == 'Pass')]
+pass_students_section_a_sorted = pass_students_section_a.sort_values(by='Percentage', ascending=False)
+top_3_students = pass_students_section_a_sorted[['Name', 'Percentage']].head(3)
+
+fail_students_section_a = df[(df['Result'] == 'Fail')]
+fail_students_section_a_sorted = fail_students_section_a.sort_values(by='Percentage')
+bottom_3_students = fail_students_section_a_sorted[['Name', 'Percentage']].head(3)
+
+col7, col8 = st.columns(2)
+
+with col7:
+    container7_1 = st.container(border=True)
+    container7_1.write("Rank 1")
+    container7_1.write(f"**{top_3_students.iloc[0]['Name']}**")   
+    container7_1.write(f"**{top_3_students.iloc[0]['Percentage']}**")
+
+    container7_2 = st.container(border=True)
+    container7_2.write("Rank 2")
+    container7_2.write(f"**{top_3_students.iloc[1]['Name']}**")   
+    container7_2.write(f"**{top_3_students.iloc[1]['Percentage']}**")
+
+    container7_3 = st.container(border=True)
+    container7_3.write("Rank 2")
+    container7_3.write(f"**{top_3_students.iloc[2]['Name']}**")   
+    container7_3.write(f"**{top_3_students.iloc[2]['Percentage']}**")
+
+with col8:
+    container8_1 = st.container(border=True)
+    container8_1.write("Rank 18")
+    container8_1.write(f"**{bottom_3_students.iloc[2]['Name']}**")   
+    container8_1.write(f"**{bottom_3_students.iloc[2]['Percentage']}**")
+
+    container8_2 = st.container(border=True)
+    container8_2.write("Rank 19")
+    container8_2.write(f"**{bottom_3_students.iloc[1]['Name']}**")   
+    container8_2.write(f"**{bottom_3_students.iloc[1]['Percentage']}**")
+
+    container8_3 = st.container(border=True)
+    container8_3.write("Rank 20")
+    container8_3.write(f"**{bottom_3_students.iloc[0]['Name']}**")   
+    container8_3.write(f"**{bottom_3_students.iloc[0]['Percentage']}**")
+
+with st.expander('Student Details'):
+    st.write(new_df.style.background_gradient(cmap="Blues"))
+
+csv = new_df.to_csv(index=False).encode('utf-8')
+st.download_button('Download Student Data', data=csv, file_name="StudentData.csv", mime="text/csv")
---
+++ pages/Section_B.py
@@ -0,0 +1,264 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+import plotly.express as px
+import pandas as pd
+
+st.set_page_config(page_title="VVS", layout="wide", initial_sidebar_state="collapsed")
+
+with open('styleB.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+df = pd.read_csv("./students.csv")
+section_b = df[df['Section'] == 'B']
+specified_columns = ['Name', 'Language', 'English', 'Maths', 'Science', 'Social', 'Percentage', 'Result',  'Gender']
+new_df = section_b[specified_columns].copy()
+
+
+st.title("Section B Analysis")
+st.markdown("<style>div.block-container{padding-top:1rem;}</style>", unsafe_allow_html=True)
+
+st.text("")
+
+col0_1, col0_2, col0_3 = st.columns([8, 2, 2])
+
+with col0_3:
+    if st.button("Dashboard"):
+        switch_page("Dashboard")  
+
+st.subheader("Subject Wise Result Analysis")
+
+subjects = ['Maths', 'Science', 'Social', 'Language']
+
+def pie(subject):
+    pass_students = section_b[(section_b[subject] >= 40)]
+    fail_students = section_b[(section_b[subject] < 40)]
+    
+    pass_count = len(pass_students)
+    fail_count = len(fail_students)
+    
+    title = f'{subject}'
+
+    labels = ['Pass', 'Fail']
+    values = [pass_count, fail_count]
+    fig = px.pie(names=labels, values=values, title=title, hole=0.5)
+    fig.update_traces(text = labels, textposition="outside")
+    st.plotly_chart(fig, use_container_width=True)
+
+col1_1, col1_2, col1_3,col1_4, col1_5 = st.columns((5))
+
+with col1_1:
+    df['Pass/Fail'] = df['Language'].apply(lambda x: 'Pass' if x >= 40 else 'Fail')
+    pie(subject = "Language")
+
+with col1_2:
+    df['Pass/Fail'] = df['English'].apply(lambda x: 'Pass' if x >= 40 else 'Fail')
+    pie(subject = "English")   
+
+with col1_3:
+    df['Pass/Fail'] = df['Maths'].apply(lambda x: 'Pass' if x >= 40 else 'Fail')
+    pie(subject = "Maths")
+
+with col1_4:
+    df['Pass/Fail'] = df['Science'].apply(lambda x: 'Pass' if x >= 40 else 'Fail')
+    pie(subject = "Science")  
+
+with col1_5:
+    df['Pass/Fail'] = df['Social'].apply(lambda x: 'Pass' if x >= 40 else 'Fail')
+    pie(subject = "Social") 
+
+length_section_B = len(df[df['Section'] == 'B'])
+section_B_counts = df[df['Section'] == 'B']['Result'].value_counts()
+section_B_gender = df[df['Section'] == 'B']['Gender'].value_counts()
+
+# Calculate the average marks for each subject in Section A
+average_marks = []
+subjects = [ 'Language', 'English', 'Maths', 'Science', 'Social']
+for subject in subjects:
+    avg_mark = section_b[subject].mean()
+    average_marks.append(avg_mark)
+
+col1, col2 = st.columns(2, gap="medium")
+
+with col1:
+    st.subheader("Section B Results")
+    fig = px.pie(values=section_B_counts.values, names=section_B_counts.index, hole=0.5)
+    fig.update_traces(text = section_B_counts.index, textposition="outside")
+    st.plotly_chart(fig, use_container_width=True)
+
+with col2:
+    st.subheader("Student Details")
+    st.text("")
+    st.text("")
+    st.text("")
+    container = st.container(border=True)
+    container.write(f"**Total Number of Students: {length_section_B}**")
+
+    col1_1, col1_2 = st.columns((2))
+    
+    with col1_1:
+        container2_1 = st.container(border=True)
+        container2_1.write("**Boys**")
+        container2_1.write(f"**{section_B_gender['Male']}**")
+
+    with col1_2:
+        container2_2 = st.container(border=True)
+        container2_2.write("**Girls**")
+        container2_2.write(f"**{section_B_gender['Female']}**")
+
+    with col1_1:
+        container2_1 = st.container(border=True)
+        container2_1.write("**Total Number of Pass**")
+        container2_1.write(f"**{section_B_counts['Pass']}**")
+
+    with col1_2:
+        container2_2 = st.container(border=True)
+        container2_2.write("**Total Number of Fails**")
+        container2_2.write(f"**{section_B_counts['Fail']}**")
+
+st.text("")
+st.subheader("Grades")
+
+col3_1, col3_2, col3_3, col3_4, col3_5, col3_6 = st.columns(6)
+col3list = [col3_1, col3_2, col3_3, col3_4, col3_5, col3_6]
+
+# Calculate the number of students in each grade range for Section A
+students_90_above = section_b[section_b['Percentage'] >= 90]
+num_students_90_above = len(students_90_above)
+
+students_80_above = section_b[(section_b['Percentage'] >= 80) & (section_b['Percentage'] < 90)]
+num_students_80_above = len(students_80_above)
+
+students_70_above = section_b[(section_b['Percentage'] >= 70) & (section_b['Percentage'] < 80)]
+num_students_70_above = len(students_70_above)
+
+students_60_above = section_b[(section_b['Percentage'] >= 60) & (section_b['Percentage'] < 70)]
+num_students_60_above = len(students_60_above)
+
+students_40_above = section_b[(section_b['Percentage'] >= 40) & (section_b['Percentage'] < 60)]
+num_students_40_above = len(students_40_above)
+
+students_40_below = section_b[section_b['Percentage'] < 40]
+num_students_40_below = len(students_40_below)
+
+# Display the grades in each column
+for i in col3list:
+    with i:
+        container = st.container(border=True)
+
+        if i is col3_1:
+            container.write("**S Grade**")
+            container.write(f"**{num_students_90_above}**")
+
+        if i is col3_2:
+            container.write("**A Grade**")
+            container.write(f"**{num_students_80_above}**")
+
+        if i is col3_3:
+            container.write("**B Grade**")
+            container.write(f"**{num_students_70_above}**")
+
+        if i is col3_4:
+            container.write("**C Grade**")
+            container.write(f"**{num_students_60_above}**")
+
+        if i is col3_5:
+            container.write("**D Grade**")
+            container.write(f"**{num_students_40_above}**")
+
+        if i is col3_6:
+            container.write("**X Grade**")
+            container.write(f"**{num_students_40_below}**")
+
+st.text("")
+st.text("")
+st.text("")
+
+col3, col4 = st.columns(2, gap="medium")
+
+with col3:
+    st.subheader("Subject Wise Average Marks")
+    st.text("")
+    st.text("")
+    st.text("")
+    col3_1, col3_2 = st.columns((2))
+    
+    with col3_1:
+        container2_1 = st.container(border=True)
+        container2_1.write("**Language**")
+        container2_1.write(f"**{average_marks[0]}**")
+
+    with col3_2:
+        container2_2 = st.container(border=True)
+        container2_2.write("**English**")
+        container2_2.write(f"**{average_marks[1]}**")
+
+    with col3_1:
+        container2_1 = st.container(border=True)
+        container2_1.write("**Maths**")
+        container2_1.write(f"**{average_marks[2]}**")
+
+    with col3_2:
+        container2_2 = st.container(border=True)
+        container2_2.write("**Science**")
+        container2_2.write(f"**{average_marks[3]}**")
+
+    with col3_1:
+        container2_1 = st.container(border=True)
+        container2_1.write("**Social Science**")
+        container2_1.write(f"**{average_marks[4]}**")
+
+with col4:
+    st.subheader("Graphical Analysis")
+    fig = px.bar(x=subjects, y=average_marks, labels={'x': 'Subject', 'y': 'Average Marks'})
+    st.plotly_chart(fig, use_container_width=True)
+
+
+st.subheader("Leader Board")
+
+pass_students_section_b = df[(df['Section'] == 'B') & (df['Result'] == 'Pass')]
+pass_students_section_b_sorted = pass_students_section_b.sort_values(by='Percentage', ascending=False)
+top_3_students = pass_students_section_b_sorted[['Name', 'Percentage']].head(3)
+
+fail_students_section_b = df[(df['Section'] == 'B') & (df['Result'] == 'Fail')]
+fail_students_section_b_sorted = fail_students_section_b.sort_values(by='Percentage')
+bottom_3_students = fail_students_section_b_sorted[['Name', 'Percentage']].head(3)
+
+col5, col6 = st.columns(2)
+
+with col5:
+    container5_1 = st.container(border=True)
+    container5_1.write("Rank 1")
+    container5_1.write(f"**{top_3_students.iloc[0]['Name']}**")   
+    container5_1.write(f"**{top_3_students.iloc[0]['Percentage']}**")
+
+    container5_2 = st.container(border=True)
+    container5_2.write("Rank 2")
+    container5_2.write(f"**{top_3_students.iloc[1]['Name']}**")   
+    container5_2.write(f"**{top_3_students.iloc[1]['Percentage']}**")
+
+    container5_3 = st.container(border=True)
+    container5_3.write("Rank 2")
+    container5_3.write(f"**{top_3_students.iloc[2]['Name']}**")   
+    container5_3.write(f"**{top_3_students.iloc[2]['Percentage']}**")
+
+with col6:
+    container6_1 = st.container(border=True)
+    container6_1.write("Rank 18")
+    container6_1.write(f"**{bottom_3_students.iloc[2]['Name']}**")   
+    container6_1.write(f"**{bottom_3_students.iloc[2]['Percentage']}**")
+
+    container6_2 = st.container(border=True)
+    container6_2.write("Rank 19")
+    container6_2.write(f"**{bottom_3_students.iloc[1]['Name']}**")   
+    container6_2.write(f"**{bottom_3_students.iloc[1]['Percentage']}**")
+
+    container6_3 = st.container(border=True)
+    container6_3.write("Rank 20")
+    container6_3.write(f"**{bottom_3_students.iloc[0]['Name']}**")   
+    container6_3.write(f"**{bottom_3_students.iloc[0]['Percentage']}**")
+
+with st.expander('Student Details'):
+    st.write(new_df.style.background_gradient(cmap="Blues"))
+
+csv = new_df.to_csv(index=False).encode('utf-8')
+st.download_button('Download Section B Student Data', data=csv, file_name="StudentData_B.csv", mime="text/csv")
